f12678bff2a8822bfa44c218835dc425d0d6452f
Gabriel Acosta Belchior <gabriel.acostab@hotmail.com>
2017-08-25 22:27:40 -0300

    Started Paint

---
+++ EDA1.MiniPaint/Colorir.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Text;
+using System.Drawing;
+
+namespace Paint
+{
+    public class Pintor
+    {
+
+        #region Implementar
+
+        // Função que pinta a imagem
+        public int Colorir(int x, int y, Color C1, Bitmap b)
+        {
+            /* Dicas: 
+             *  - Pode-se colocar na fila x e y. ou pode-se colocar direto um ponto. Pode-se usar a classe Point para isso. Ex,: Point p = new Point(x,y)
+             *  - Uma variável que armazena cor é do tipo Color. Ex.: Color cor_ponto
+             *  - Para pegar uma cor de um ponto do Bitmap use: b.GetPixel(x,y)
+             *  - Para gravar uma cor em um ponto do Bitmap use: b.SetPixel(x,y,cor)
+             * - Uma cor é igual a outra se seus 3 canais (R, G e B) forem iguais
+             * */
+
+            Fila f = new Fila(b.Size.Width * b.Size.Height);
+            Color LOriginalColor = b.GetPixel(x, y);
+            int LNumPixels = 0;
+            f.Insert(new Point(x, y));
+
+            while (!f.Empty())
+            {
+                Point p = (Point)f.Remove();
+                if (b.GetPixel(p.X - 1, p.Y) == LOriginalColor) f.Insert(new Point(p.X - 1, p.Y));
+                if (b.GetPixel(p.X + 1, p.Y) == LOriginalColor) f.Insert(new Point(p.X + 1, p.Y));
+                if (b.GetPixel(p.X, p.Y - 1) == LOriginalColor) f.Insert(new Point(p.X, p.Y - 1));
+                if (b.GetPixel(p.X, p.Y + 1) == LOriginalColor) f.Insert(new Point(p.X, p.Y + 1));
+
+                b.SetPixel(p.X, p.Y, C1);
+                LNumPixels++;
+            }
+
+            return LNumPixels;
+        }
+
+        #endregion
+    }
+}
---
+++ EDA1.MiniPaint/Fila.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Paint
+{
+
+    public class Fila
+    {
+        private int size, front, rear, count;
+        private object[] elements;
+
+        public Fila(int size)
+        {
+            this.size = size;
+            front = rear = count = 0;
+            elements = new object[size];
+        }
+
+        public bool Empty()
+        {
+            return count == 0;
+        }
+
+        public bool Full()
+        {
+            return count == size - 1;
+        }
+
+        public void Insert(object x)
+        {
+            if (Full()) throw new StackOverflowException("Fila Cheia");
+            rear = (rear == size - 1) ? 0 : rear + 1;
+            elements[rear] = x;
+            count++;
+        }
+
+        public object Remove()
+        {
+            if (Empty()) throw new InvalidOperationException("Fila Vazia");
+            front = (front == size - 1) ? 0 : front + 1;
+            count--;
+            return elements[front];
+        }
+
+    }
+}
